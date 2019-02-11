40db3f16b5254b89494865e6e522af661988600c
FullstackDeveloper512 <akagishotaro44@gmail.com>
2019-02-11 23:13:14 +0900

    hello?

--- src/App.js
+++ src/App.js
@@ -5,22 +5,28 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className="gray-backgroud">
+          <img src={logo} lat="logo"/>
+          <h2>Let's develop management system!</h2>
+        </div>
+
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Hello React Project
+      //       {/* Edit <code>src/App.js</code> and save to reload. */}
+      //     </p>
+      //     {/* <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a> */}
+      //   </header>
+      // </div>
     );
   }
 }
