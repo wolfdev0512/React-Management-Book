5f007a2fab5c03e4849192d89716decc40e1f968
FullstackDeveloper512 <akagishotaro44@gmail.com>
2019-02-17 12:51:58 +0900

    Changed api

--- server.js
+++ server.js
@@ -10,10 +10,18 @@ app.use(bodyParser.urlencoded({
     extended : true
 }));
 
-app.get('/api/hello', (req, res) => {
-res.send(
-    {message: 'Hello Express'}
-)
+// app.get('/api/hello', (req, res) => {
+// res.send(
+//     {message: 'Hello Express'}
+// )
+// });
+
+app.get('/api/customers', (req, res)=>{
+    //요청이 들어왔을떄 응답하는 REST API 
+    res.send([
+
+    ]);
 });
+
 
 app.listen(port, ()=>console.log('Listeneing on port ${port}'));
